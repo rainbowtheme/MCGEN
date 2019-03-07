70d175ce86bd2759a4c66f6ca55828b5b69307a9
rainbowtheme <43517515+rainbowtheme@users.noreply.github.com>
2019-03-07 15:15:18 +0700

    Update code.py

--- code.py
+++ code.py
@@ -10,25 +10,44 @@ from discord import Game
 Client = discord.client
 client = commands.Bot(command_prefix = '!')
 Clientdiscord = discord.Client()
-client.remove_command('help')
+
+Category = "553028002213265414"
 
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=discord.Game(name='Type !gen'))
-    print('Bot is ready.')
+    await client.change_presence(game=discord.Game(name='BlueGaming', type = 3))
+    print('Siap Bos')
+    
 
-@client.event
-async def on_member_join(member):
-    role = discord.utils.get(member.server.roles, name='Members')
-    await client.add_roles(member, role)
+@client.command(pass_context=True)
+async def hi(ctx):
+    await client.say("Hello {} :joy:".format(ctx.message.author.name))
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('!gen'):
-        randomlist = ['emiliogiraltnorro@hotmail.com:emilio2003','felix@methe.de:felix1505','mangysage@gmail.com:Scoobi71@','pashodidi@abv.bg:Husjern20','peyjames13@gmail.com:gobears34','philipp.rahrbach@gmx.de:Philipp2003!!...','pratt.trevor@rocketmail.com:s1937tap','enco.juricev555@gmail.com:1234enco','christenson03@gmail.com:Samuel03','perezisaiah843@gmail.com:hectorxd27','s.ribich95@gmail.com:steben01256!','rembrandt132010@gmail.com:66018530','ludvigekelof@gmail.com:kattenbosse1','martinbruraas@gmail.com:Beretta1','hellespont24@hotmail.com:Exia2012','brown_521@msn.com:Monty123','knoll1749@charter.net:Thunder523','xbungalowx@gmail.com:Jailbird2','seanjean1324@gmail.com:killerjet57']
-        await client.send_message(message.author,(random.choice(randomlist)))
-        await client.send_message(message.channel,('Check Your DM :smiley:'))
-        await client.send_message(message.author,'Bot by @Blue Gaming#5147')
-        
-client.run('TOKEN')
+@client.command(pass_context=True)
+async def echo(ctx, *,msg):
+    await client.say(msg)
+
+@client.command(pass_context=True)
+async def embed(ctx):
+    embed = discord.Embed(title="Help", color=0x000000)
+    embed.add_field(name="gen", value='Your account', inline=False)
+    embed.set_footer(text="Get Your account {}".format(ctx.message.author.name))
+    await client.say(embed=embed)
+
+    
+@client.command(pass_context=True)
+async def gen(ctx):
+    randomlist = ['bcs5000@live.com:123zaqccx','devgrant9@gmail.com:kipper1','owenpaine15@gmail.com:dudewolfer1','lateriss@gmail.com:Pinhead14$$','dan_jensen91@hotmail.com:fuckups134','niegsch.j@gmail.com:Finnie346643','tamsinwilson2001@gmail.com:jazzthedog','kaolakid@yahoo.com:pc198800','katje_mieke@hotmail.com:1408KILL','shahzilkhokhar@hotmail.com:Shamir007','mynameismikals@gmail.com:230811as','swiffet@gmail.com:ender2007']
+    embed = discord.Embed(title="Account Minecraft", color=0x000000)
+    embed.add_field(name="Your account", value=random.choice(randomlist), inline=False)
+    embed.add_field(name="Published By", value="Blue Gaming#5147", inline=False)
+    embed.set_footer(text="Thanks {}".format(ctx.message.author.name))
+    await client.send_message(ctx.message.author, embed=embed)
+    await client.send_message(ctx.message.channel, "Check Your Dms")
+
+
+    
+
+
+client.run('TOKEN')  
